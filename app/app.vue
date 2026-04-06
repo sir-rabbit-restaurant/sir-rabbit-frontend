c55c9689159a59d51adea61ae7ff706991b1af4f
evg <zhenya_timofeev_99_99@mail.ru>
2026-04-06 15:36:34 +0300

    вытянул карту

--- app/app.vue
+++ app/app.vue
@@ -2,7 +2,8 @@
   <TheHeader :logo="getImageUrl(siteInfo.logo)" />
   <HeroSection :name="info?.name" />
   <MenuSection />
-  <Contacts :schedule="info?.schedule":address="info?.address" :phone="info?.contacts" :map="info?.yandex_maps_iframe" />
+  <Contacts :schedule="info?.schedule" :address="info?.address" :phone="info?.contacts"
+    :map-code="info?.yandex_maps_iframe" />
   <NuxtPage />
 </template>
 
@@ -16,15 +17,23 @@ const { data: siteInfo, pending } = await useAsyncData('site_info', () =>
     params: {
       fields: ['id', 'name', 'address', 'contacts', 'links', 'logo', 'yandex_maps_iframe', 'schedule'],
     }
+    
   })
+
 );
 
 
-const info = computed(() => siteInfo.value || null);
+
+
+
+const info = computed(() => siteInfo.value|| null);
 
 const config = useRuntimeConfig();
 const getImageUrl = (id) => {
   console.log(siteInfo.value.logo);
   return id ? `${config.public.directus.url}/assets/${id}?quality=10` : null;
 }
+
+console.log('--- ДАННЫЕ ИЗ DIRECTUS ---');
+console.log('Объект целиком:', info?.yandex_maps_iframe);
 </script>
