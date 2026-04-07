a4e4808f8af0c6b73950bd1884749b2fcba13a60
Katy248 <petrovanton247@gmail.com>
2026-04-08 01:38:55 +0300

    Change page

--- app/app.vue
+++ app/app.vue
@@ -1,21 +1,13 @@
 <template>
     <TheHeader v-if="siteInfo" :logo="getImageUrl(siteInfo.logo)" />
-    <HeroSection v-if="siteInfo" :name="siteInfo.name" />
-    <MenuSection />
-    <Contacts
-        v-if="siteInfo && contacts"
-        :schedule="siteInfo.schedule"
-        :address="siteInfo.address"
-        :contacts="contacts"
-        :map-code="siteInfo.yandex_maps_iframe"
-    />
-    <TheFooter v-if="siteInfo && contacts && links"
-        :schedule="siteInfo.schedule"
-        :address="siteInfo.address"
-        :contacts="contacts"
-        :links="links"        
-        />
     <NuxtPage />
+    <TheFooter
+        v-if="siteInfo && contacts && links"
+        :schedule="siteInfo.schedule"
+        :address="siteInfo.address"
+        :contacts="contacts"
+        :links="links"
+    />
 </template>
 
 <script setup lang="ts">
@@ -24,7 +16,7 @@ import type { SiteInfo, Link, Contact } from "./models";
 const { getItems, getSingletonItem } = useDirectusItems();
 
 // Вызываем данные. Называем переменную 'siteData', чтобы не путать с меню
-const { data: siteInfo, pending } = await useAsyncData("site_info", () =>
+const { data: siteInfo } = await useAsyncData("site_info", () =>
     getSingletonItem<SiteInfo>({
         collection: "site_info",
     }),
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,4 +1,27 @@
 <template>
-    
+    <HeroSection v-if="siteInfo" :name="siteInfo.name" />
+    <MenuSection />
+    <Contacts
+        v-if="siteInfo && contacts"
+        :schedule="siteInfo.schedule"
+        :address="siteInfo.address"
+        :contacts="contacts"
+        :map-code="siteInfo.yandex_maps_iframe"
+    />
 </template>
 
+<script setup lang="ts">
+import type { SiteInfo, Contact } from "~/models";
+
+const { getItems, getSingletonItem } = useDirectusItems();
+
+const { data: siteInfo } = await useAsyncData("site_info", () =>
+    getSingletonItem<SiteInfo>({
+        collection: "site_info",
+    }),
+);
+
+const { data: contacts } = await useAsyncData("contacts", () =>
+    getItems<Contact>({ collection: "contact" }),
+);
+</script>
